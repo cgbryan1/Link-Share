4034f850aa1b2ff30e287122c4fdeb7f5eb26ffa
Caroline Bryan <cgbryan@unc.edu>
2025-03-07 04:05:51 +0000

    working on part 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { ShareComponent } from './share/share.component';
+import { ResourcesComponent } from './resources/resources.component';
 
-export const routes: Routes = [{ path: '', component: ShareComponent }];
+export const routes: Routes = [
+  { path: '', component: ShareComponent },
+  { path: 'new-resource', component: ResourcesComponent },
+];
